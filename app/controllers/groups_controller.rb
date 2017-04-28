520f826a9e8a106ecad96d461a25b7da15c9590c
KayiSoft <wenzhengde@me.com>
2017-04-28 08:09:14 +0800

    add permission check on show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,33 +1,32 @@
 class GroupsController < ApplicationController
-
+   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy] 
   def index
     @groups = Group.all
   end
   def new
       @group = Group.new
   end
+
   def destroy
-       @group= Group.find(params[:id])
-       @group.destroy
-       flash[:alert] = "Group deleted"
-       redirect_to groups_path
+    @group.destroy
+    redirect_to groups_path, alert: "Group deleted"
   end
-  
+ 
   def show
       @group= Group.find(params[:id])
   end
-  def edit
-      @group = Group.find(params[:id])
-  end
-  def update
-      @group = Group.find(params[:id])
-      if  @group.update(group_params)
-         redirect_to group_path,notice:"Update Success"
-      else 
-         render :edit
-      end
-  end
 
+  def edit
+  end 
+  
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
  
   def create
       @group= Group.new(group_params)
@@ -39,10 +38,15 @@ class GroupsController < ApplicationController
       end
   end
 
-  private
+private
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
 
   def group_params
-      params.require(:group).permit(:title,:description)
+    params.require(:group).permit(:title, :description)
   end
- before_action :authenticate_user! , only: [:new, :create]
 end
